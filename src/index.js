739a8ea6734d78aedecff5f063a95df0b3e3a7a0
professor-oats <chronophob@gmail.com>
2024-12-07 21:21:53 +0100

    casual tryout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Box from './3Dbox';
+import BookPage from './BookPage';
 import {Canvas} from "@react-three/fiber";
+import { Text } from '@react-three/drei';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Canvas>
+    <Canvas
+      camera={{ position: [0, 0, 1], fov: 65 }}
+      style={{ width: '100%', height: '100vh' }}
+    >
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <BookPage position={[0, 0, 0]} />
+      <BookPage position={[0.1, 0, 0.1]} />
+      <Text
+        position={[0, 0, 0.2]}
+        fontSize={0.2}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+      >
+        Hello Mom!!
+      </Text>
     </Canvas>,
   </React.StrictMode>
 );
